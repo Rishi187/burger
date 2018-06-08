105f28d3c443848536732483135a7e960eccc120
Monsoon Co <monsoonco@Monsoons-MacBook-Pro.local>
2018-06-07 20:20:53 -0700

    final pull all files updated

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db
+INSERT INTO burgers(burger_name) VALUES ('Double Double Burger');
+INSERT INTO burgers(burger_name) VALUES ( 'Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Big Mac Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Turkey Hamburger');
